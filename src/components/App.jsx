71520be451f87881e0424755f71988a11f3bf9eb
Ed <silva.email@yahoo.com>
2024-03-04 15:49:41 -0500

    Add name input and submit functionality to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+const [name, setName] = useState("")
+
+const [click, setClick] = useState("")
+
+  const handleChange = () => {
+
+setName(e.target.value)
+
+  }
+const handleClick = (e) => {
+  setClick(name)
+}
+
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {click} </h1>
+      <input onChange={handleChange}
+       type="text" 
+       placeholder="What's your name?"
+       value={click} />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }
